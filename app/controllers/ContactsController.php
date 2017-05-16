b30103a73f405fc7ea44a2b5401eb2e58a3e9e19
Guillaume Caggia <guillaume.caggia@gmail.com>
2017-05-17 03:43:29 +1000

    use of layout basic.phtml for the app

--- app/controllers/ContactsController.php
+++ app/controllers/ContactsController.php
@@ -4,6 +4,11 @@ use Phalcon\Mvc\Controller;
 
 class ContactsController extends Controller {
     
+    public function initialize()
+    {
+        $this->view->setTemplateAfter("basic");
+    }
+    
     public function indexAction()
     {
         $this->view->contacts = Contacts::find();

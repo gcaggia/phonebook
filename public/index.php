ce5fc06ea94fc1a44317ad7a6e679610b9fe7980
Guillaume Caggia <guillaume.caggia@gmail.com>
2017-05-16 11:53:06 +1000

    Phalcon bootstrap index.php file

--- public/index.php
+++ public/index.php
@@ -1,3 +1,41 @@
 <?php
 
-echo "<h1>Hello World</h1>";
+use Phalcon\Loader;
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Application;
+use Phalcon\Mvc\Url as UrlProvider;
+
+try {
+    
+    // Register the loader component
+    $loader = new Loader();
+    $loader->registerDirs(array(
+        '../app/controllers',
+        '../app/models'
+    ))->register();
+    
+    // DI Container
+    $di = new FactoryDefault();
+    
+    // Register the view service
+    $di->set('view', function() {
+        $view = new View();
+        $view->setViewsDir('../app/views');
+        return $view;
+    });
+    
+    // Register the URL service 
+    $di['url'] = function() {
+        $url = new UrlProvider();
+        $url->setBaseUri('/phonebook/');
+        return $url;
+    };
+    
+    // handle requests
+    $app = new Application($di);
+    echo $app->handle()->getContent();
+    
+} catch( \Exception $e) {
+    echo "Exception: ", $e->getMessage();
+}
